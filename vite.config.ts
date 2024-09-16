c04a6da4b3e179de99d0590cb0c3942694997cef
iamdaichi <takanogi2468@gmail.com>
2024-09-16 07:59:43 +0100

    under construction

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/typescript-react-bootstrap-portfolio/",
+  base:"/typescript-portfolio3/",
   plugins: [react()],
   optimizeDeps: {
     include: ['@vis.gl/react-google-maps']
